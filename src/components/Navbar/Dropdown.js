0e2f784c203a1fa8500172c8896a4a583aaeab77
Naina012 <106829106+Naina012@users.noreply.github.com>
2023-11-12 08:48:12 +0530

    responsive

--- src/components/Navbar/Dropdown.js
+++ src/components/Navbar/Dropdown.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Dropdown } from 'react-bootstrap';
-// import "./Dropdown.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
-function CustomDropdown({ title, items }) {
+import "./Navbar.css"
+function CustomDropdown({ title, items, onClick }) {
   return (
     <Dropdown>
-      <Dropdown.Toggle  id="dropdown-basic" style={{ backgroundColor: "#478ac9" }}>
+      <Dropdown.Toggle id="dropdown-basic" style={{ backgroundColor: "#478ac9" }}>
         {title}
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
         {items.map((item, index) => (
-          <Dropdown.Item key={index}>
-            <Link to={item.path} className={item.cName}>
+          <Dropdown.Item key={index} className='dropdown-links'>
+            <Link to={item.path} className={item.cName} onClick={onClick} >
               {item.title}
             </Link>
           </Dropdown.Item>
